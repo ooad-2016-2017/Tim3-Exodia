456316ce8736c8a82247803834319bc366662430
Enis Ahmetović <eahmetovic2@etf.unsa.ba>
2017-05-13 21:50:30 +0200

    Dodane forme za ljudske resurse

--- Projekat/FutureHotel/FutureHotel/Ljudski resursi/UserControlDatum.xaml.cs
+++ Projekat/FutureHotel/FutureHotel/Ljudski resursi/UserControlDatum.xaml.cs
@@ -13,19 +13,15 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace FutureHotel
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class UserControlDatum : UserControl
     {
-        public MainPage()
+        public UserControlDatum()
         {
             this.InitializeComponent();
         }
-        
     }
 }
