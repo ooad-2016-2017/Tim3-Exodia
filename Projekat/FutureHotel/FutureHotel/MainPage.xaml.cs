b748b83401ea599067d90209147c3c7595327bfa
Hamza Iseric <hiseric1@etf.unsa.ba>
2017-05-20 16:04:26 +0200

    Modifikacija klasa

--- Projekat/FutureHotel/FutureHotel/MainPage.xaml.cs
+++ Projekat/FutureHotel/FutureHotel/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using FutureHotel.Ljudski_resursi;
+using FutureHotel.Restoran;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -46,6 +47,11 @@ namespace FutureHotel
                 this.ButtonLjudskiResursi.FontSize = Math.Floor(fontsizeMultiplier); 
 
             }
+        }
+
+        private void ButtonRestoranClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Predjelo));
         }
     }
 }
