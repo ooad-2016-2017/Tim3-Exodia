794ec409b606171d7e6b757a0b3d579c166c7f56
Hamza Iseric <hiseric1@etf.unsa.ba>
2017-05-21 23:39:23 +0200

    Baza

---
+++ Projekat/FutureHotel/FutureHotel/Model/RestoranBaza.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace FutureHotel.Model
+{
+    class RestoranBaza : DbContext
+    {
+        public DbSet<Narudzba> Restorani { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string databaseFilePath = "Ooadbaza.db";
+            try
+            {
+                //za tačnu putanju gdje se nalazi baza uraditi ovdje debug i procitati Path
+                databaseFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, databaseFilePath);
+            }
+            catch (InvalidOperationException) { }
+            //Sqlite baza
+            optionsBuilder.UseSqlite($"Data source={databaseFilePath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //jedno od polja je image da se zna šta je zapravo predstavlja byte[]
+           // modelBuilder.Entity<Narudzba>().Property(p => p.Slika).HasColumnType("image");
+        }
+    }
+}
