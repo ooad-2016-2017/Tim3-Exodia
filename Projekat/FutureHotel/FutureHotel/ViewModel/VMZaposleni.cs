678501e76fc51a825eaa6fd3432a3ac76f67e208
mehmed-aljic <maljic1@etf.unsa.ba>
2017-05-21 23:50:35 +0200

    Validacije ljudskih resursa

--- Projekat/FutureHotel/FutureHotel/ViewModel/VMZaposleni.cs
+++ Projekat/FutureHotel/FutureHotel/ViewModel/VMZaposleni.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.UI.Popups;
@@ -16,10 +17,36 @@ namespace FutureHotel.ViewModel
         public DateTime datum { get; set; }
         public String plata { get;set; }
         public ICommand komanda { get; set; }
+        public bool validirano { get; set; }
 
         public VMZaposleni()
         {
             komanda = new RelayCommand<object>(dodajZaposlenog, moze);
+        }
+
+        public async void validiraj()
+        {
+            Regex r = new Regex("^[a-z A-Z]+$");
+            if (string.IsNullOrEmpty(ime) || !r.IsMatch(ime))
+            {
+                var dialog = new MessageDialog("Neispravno ime");
+                await dialog.ShowAsync();
+                validirano = false; return;
+            }
+            if (string.IsNullOrEmpty(prezime) || !r.IsMatch(prezime))
+            {
+                var dialog = new MessageDialog("Neispravno prezime");
+                await dialog.ShowAsync();
+                validirano = false; return;
+            }
+            r = new Regex("^[0-9]+$");
+            if (string.IsNullOrEmpty(plata) || !r.IsMatch(plata))
+            {
+                var dialog = new MessageDialog("Neispravna plata");
+                await dialog.ShowAsync();
+                validirano = false; return;
+            }
+            validirano = true;
         }
 
         public bool moze(object param)
@@ -29,9 +56,13 @@ namespace FutureHotel.ViewModel
 
         public async void dodajZaposlenog(object param)
         {
-            //rad sa bazom
-            var dialog = new MessageDialog("Zaposleni uspjesno dodan!");
-            await dialog.ShowAsync();
+            validiraj();
+            if (validirano)
+            {
+                //rad sa bazom
+                var dialog = new MessageDialog("Zaposleni uspjesno dodan!");
+                await dialog.ShowAsync();
+            }
         }
     }
 }
