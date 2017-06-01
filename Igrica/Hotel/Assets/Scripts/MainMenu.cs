66f5ae973a81734684dca4d56a1d1d3ccf7ed82a
Enis Ahmetović <eahmetovic2@etf.unsa.ba>
2017-06-01 21:19:35 +0200

    igrica

---
+++ Igrica/Hotel/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public Text scoreText;
+
+    private void Start()
+    {
+        scoreText.text = PlayerPrefs.GetInt("score").ToString();
+    }
+	
+	public void ToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
